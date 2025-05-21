c19314d265cfa341a70f478e620d242f21d8b5b3
Charles <66703824+chardynamics@users.noreply.github.com>
2025-05-21 10:10:39 -0400

    Initial Commit

---
+++ Allocate/AllocateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllocateApp.swift
+//  Allocate
+//
+//  Created by Charles Duong on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AllocateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllocateTests/AllocateTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllocateTests.swift
+//  AllocateTests
+//
+//  Created by Charles Duong on 5/21/25.
+//
+
+import Testing
+@testable import Allocate
+
+struct AllocateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
